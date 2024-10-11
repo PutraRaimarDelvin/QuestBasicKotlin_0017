09ae489e3b436c0ac5ae4fdf02e1126e37540ab0
AryaGGWP <arya.lampung.2016@gmail.com>
2024-10-12 01:31:11 +0700

    8 commit menamai collections

---
+++ app/src/main/java/com/example/noactivity/Collections.kt
@@ -0,0 +1,7 @@
+package com.example.noactivity
+
+// List adalah Kumpulan data yang disusun secara berurutan
+// List bersifat ordered, artinya data yang dimasukkan memiliki posisi sesuai urutan
+// List bersifat mutable dan read only, artinya data yang dimasukka dapat diubah dan tidak dapat diubah
+// List Read-Only menggunakan listOf
+// List Mutable menggunakan mutableListOf
