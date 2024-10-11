229938be96decaf12b460f73fcee05ad9d59f81e
AryaGGWP <arya.lampung.2016@gmail.com>
2024-10-12 01:36:18 +0700

    10 commit menambahkan catatan

--- app/src/main/java/com/example/noactivity/ConditionalExpression.kt
+++ app/src/main/java/com/example/noactivity/ConditionalExpression.kt
@@ -1,1 +1,5 @@
 package com.example.noactivity
+
+// Conditional Expression
+// Conditional expression adalah ekspresi yang mengembalikan nilai berdasarkan kondisi tertentu
+// Conditiona Expression menggunakan if-else, when, dan try-catch
