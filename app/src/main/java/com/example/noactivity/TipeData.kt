b92271ce64edca74496f8640cc8386d14c17af7a
AryaGGWP <arya.lampung.2016@gmail.com>
2024-10-12 01:23:41 +0700

    2 commit to data

---
+++ app/src/main/java/com/example/noactivity/TipeData.kt
@@ -0,0 +1,17 @@
+package com.example.noactivity
+
+class TipeData {
+}
+
+fun main() {
+    // Byte
+    val byteValue: Byte = 10
+    println("Byte: $byteValue")
+
+    // Short
+    val shortValue: Short = 20
+    println("Short: $shortValue")
+
+    // Int
+    val intValue: Int = 30
+    println("Int: $intValue")
